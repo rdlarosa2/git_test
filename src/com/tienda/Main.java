b7dfc63ac42dd9c38ec060e2a1b2100baa67cdbc
Ricardo <rdlarosa2@gmail.com>
2020-08-30 18:51:21 -0500

    v1_0_0

---
+++ src/com/tienda/Main.java
@@ -0,0 +1,13 @@
+package com.tienda;
+
+public class Main {
+
+	public static void main(String[] args) {
+        System.out.println("Primera version");
+        
+        System.out.println("Este es un hotfix");        
+        
+        System.out.println("feature1");
+	}
+
+}
